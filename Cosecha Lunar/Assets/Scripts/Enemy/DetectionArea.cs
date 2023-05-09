67114dd9a64d236afc3cea5b55fa07bc3348adab
Diegoaap24jun <diego24jun@gmail.com>
2023-05-09 15:32:10 -0500

    Code of Enemy2 set in the proyect
    We added more lines to Enemy2 can move when "Player" is near to him

--- Cosecha Lunar/Assets/Scripts/Enemy/DetectionArea.cs
+++ Cosecha Lunar/Assets/Scripts/Enemy/DetectionArea.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class DetectionArea : MonoBehaviour
 {
 
     [SerializeField] private LayerMask playerLayerMask;
+    [SerializeField] private float distance;
     bool detected;
     GameObject target;
     public Transform enemy;
@@ -15,16 +17,59 @@ public class DetectionArea : MonoBehaviour
     public float timeToShoot = 1.3f;
     float originalTime;
     private bool isShooting = false;
+    private bool enableShooting = true;
+
+    [SerializeField] private List<Transform> points;
+    [SerializeField] private NavMeshAgent IA2;
+    [SerializeField] private float distance2;
+    private bool running = false;
+
 
 
     void Start()
     {
+        target = GameObject.Find("Player");
         originalTime = timeToShoot;
+
+        StartCoroutine(RunningLogic());
     }
 
+    private IEnumerator RunningLogic()
+    {
+        while (true)
+        {
+            while (Vector3.Distance(transform.position, target.transform.position) > distance2)
+            {
+                yield return null;
+            }
+            Vector3 position = points[Random.Range(0, points.Count)].position;
+            IA2.SetDestination(position);
+            IA2.isStopped = false;
+            enableShooting = false;
+            while (Vector3.Distance(transform.position, position) > 1)
+            {
+                yield return null;
+            }
+            enableShooting = true;
+            IA2.isStopped = true;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, distance);
+        Gizmos.DrawWireSphere(transform.position, distance2);
+    }
     // Update is called once per frame
     void Update()
     {
+        if (Vector3.Distance(transform.position, target.transform.position) < distance)
+        {
+            detected = true;
+        }
+        else
+        {
+            detected = false;
+        }
         if (detected)
         {
             enemy.LookAt(target.transform);
@@ -32,21 +77,21 @@ public class DetectionArea : MonoBehaviour
     }
 
     private void FixedUpdate()
-    { 
+    {
         if (detected)
         {
             timeToShoot -= Time.deltaTime;
 
             if (timeToShoot < 0)
             {
-                InvokeRepeating("ShootPlayer", 0.0f, 5.0f);
-                //ShootPlayer();
+                //InvokeRepeating("ShootPlayer", 0.0f, 5.0f);
+                StartCoroutine(ShootPlayer());
                 timeToShoot = originalTime;
             }
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    /*private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
@@ -60,24 +105,24 @@ public class DetectionArea : MonoBehaviour
         {
             detected = false;
         }
-    }
+    }*/
 
-    private void ShootPlayer()
+    private IEnumerator ShootPlayer()
     {
-        if (isShooting)
+        for (int i = 1; i <= 3; i++)
         {
-            isShooting = false;
-        }
-        else
-        {
-            GameObject currentBullet = Instantiate(bullet, shootPoint.position, shootPoint.rotation);
-            Rigidbody rig = currentBullet.GetComponent<Rigidbody>();
+            if (enableShooting)
+            {
+                GameObject currentBullet = Instantiate(bullet, shootPoint.position, shootPoint.rotation);
+                Rigidbody rig = currentBullet.GetComponent<Rigidbody>();
 
-            rig.AddForce(transform.forward * shootSpeed, ForceMode.VelocityChange);
-            Invoke("DeacivatedTurret", 5.0f);
+                rig.AddForce(transform.forward * shootSpeed, ForceMode.VelocityChange);
+                yield return new WaitForSeconds(0.25f);
+            }
 
-            isShooting = true;
         }
+
+
     }
 
     private void DeacivatedTurret()
